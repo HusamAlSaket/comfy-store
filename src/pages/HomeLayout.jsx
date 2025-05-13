f0f638f93a44abc3c106ab3e09814f1af477c1b8
HusamALSaket <alsaket.husam@gmail.com>
2025-05-13 12:29:07 +0300

    404 error

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 function HomeLayout() {
   return (
-    <div>HomeLayout</div>
+    <>
+    <nav>
+      <span className="text-4xl text-primary">Comfy</span>
+    </nav>
+    <Outlet />
+    </>
   )
 }
 
