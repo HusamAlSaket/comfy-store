47bba25f38009a59ee3361a904ac0ee1a3926874
HusamALSaket <alsaket.husam@gmail.com>
2025-05-18 13:56:09 +0300

    NavLinks +Navbar +Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
-function Navbar() {
+import React from "react";
+import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-base-200">
+      <div className="navbar align-element">
+        <div className="navbar-start">
+          {/* {TITLE} */}
+          <NavLink
+            to="/"
+            className="hidden lg:flex btn btn-primary text-3xl items-center"
+          >
+            C
+          </NavLink>
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="h6 w-6" />
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z[1] p-2 shadow bg-base-200 roundex-box w-52"
+            >
+             <NavLinks/>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">
+      <NavLinks/>
 
-export default Navbar
+          </ul>
+        </div>
+        <div className="navbar-end"></div>
+        {/* {THEME SETUP} */}
+        {/* {CART LINK} */}
+        <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
+          <div className="indicator">
+            <BsCart3 className="h6 w-6" />
+            <span className="badge badge-sm badge-primary indicator-item">
+              8
+            </span>
+          </div>
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 function HomeLayout() {
   return (
     <>
-    <nav>
-      <span className="text-4xl text-primary">Comfy</span>
-    </nav>
+    <Header />
+    <Navbar />
     <section className='align-element py-20'>
     <Outlet />
     </section>
