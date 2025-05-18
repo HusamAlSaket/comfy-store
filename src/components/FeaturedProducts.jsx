b9fa551c498065162631d53cb2532acf43ca7ab0
HusamALSaket <alsaket.husam@gmail.com>
2025-05-18 17:32:43 +0300

    sectionTitle+productsGrid+FeaturedProducts

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
-function FeaturedProducts() {
+import ProductsGrid from './ProductsGrid';
+import SectionTitle from './SectionTitle';
+const FeaturedProducts=() =>{
   return (
-    <div>FeaturedProducts</div>
+    <div className='pt-24'>
+        <SectionTitle text="Featured Products" />
+        <ProductsGrid/>
+    </div>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Hero } from "../components";
 import { customFetch } from "../utils";
-
+import FeaturedProducts from "../components/FeaturedProducts";
 const url = "/products?featured=true";
 
 export const loader = async () => {
@@ -14,6 +14,7 @@ const Landing=()=> {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   );
 }
