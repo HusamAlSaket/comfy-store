bbf6d35dc12695cbd755e4e51fdc65f60915605d
HusamALSaket <alsaket.husam@gmail.com>
2025-05-18 12:53:11 +0300

    custom class +Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {FormInput, SubmitBtn} from '../components';
+import { Form,Link} from 'react-router-dom';
 
-function Register() {
+
+const  Register= () =>{
   return (
-    <div>Register</div>
+    <section className='h-screen grid place-items-center'>
+      <Form method='post' className='card w-96 bg-base-100 shadow-xl p-8 flex flex-col 
+      gap-y-4'>
+        
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput name='username' type='text' label='username'  />
+        <FormInput name='email' type='email' label='email'  />
+        <FormInput name='password' type='password' label='password'  />
+      <div className="mt-4">
+        <SubmitBtn text='register' />
+            <p className="text-center">
+           Already A member  ?{" "}
+          <Link
+            to="/login"
+            className="ml-2 link link-hover
+            link-primary capitalize"
+          >
+            Login
+          </Link>
+        </p>
+      
+      </div>
+      </Form>
+    </section>
   )
 }
 
