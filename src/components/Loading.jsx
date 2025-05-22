2e2961848098d95b3018bee82f85529adb4125f5
HusamALSaket <alsaket.husam@gmail.com>
2025-05-22 12:19:22 +0300

    Loader

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function Loading() {
-  return (
-    <div>Loading</div>
-  )
-}
+const Loading = () => {
+  return <div className="h-screen flex items-center justify-center">
+<span className="loading loading-ring loading-xs"></span>
+<span className="loading loading-ring loading-sm"></span>
+<span className="loading loading-ring loading-md"></span>
+<span className="loading loading-ring loading-lg"></span>
 
-export default Loading
+  </div>;
+};
+
+export default Loading;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from '../components/Header'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { Outlet, useNavigation } from "react-router-dom";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 function HomeLayout() {
+  const navigation = useNavigation();
+  const isPageLaoding = navigation.state === "loading";
   return (
     <>
-    <Header />
-    <Navbar />
-    <section className='align-element py-20'>
-    <Outlet />
-    </section>
+      <Header />
+      <Navbar />
+      {isPageLaoding ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
     </>
-  )
+  );
 }
 
-export default HomeLayout
+export default HomeLayout;
